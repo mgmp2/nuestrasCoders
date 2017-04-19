bf278ee8feb469af16f34f461e3c337549d455cf
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-19 13:08:53 -0500

    create div's, img, span and title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,45 @@
-var padre = document.getElementById("body");
-var styleCoder =   [{nombre: "Naomi Villanueva", url: "../img/students/1.png"},
-                    {nombre: "Ana Durand", url: "../img/students/2.png"},
-                    {nombre: "Betsi Loayza", url: "../img/students/3.png"},
-                    {nombre: "Maia Rojas", url: "../img/students/4.png"},
-                    {nombre: "Miriam Mendoza", url: "../img/students/5.png"},
-                    {nombre: "Neiza Nuñez", url: "../img/students/6.png"}];
+var padre = document.getElementById("container");
+var styleCoder =   [{nombre: "Naomi Villanueva", url: "assets/img/students/1.png"},
+                    {nombre: "Ana Durand", url: "assets/img/students/2.png"},
+                    {nombre: "Betsi Loayza", url: "assets/img/students/3.png"},
+                    {nombre: "Maia Rojas", url: "assets/img/students/4.png"},
+                    {nombre: "Miriam Mendoza", url: "assets/img/students/5.png"},
+                    {nombre: "Neiza Nuñez", url: "assets/img/students/6.png"}];
+var dcf = document.createDocumentFragment();
 window.addEventListener("load", function (e){
   e.preventDefault();
-  console.log(styleCoder[2].nombre);
-  console.log(styleCoder[2].url);
-
-
+  for (var i = 0; i < styleCoder.length; i++) {
+    createCoders(styleCoder[i].nombre, styleCoder[i].url);
+  }
+  createtitle(dcf,padre);
 })
+
+
+function createCoders(nombre, url){
+  var div = document.createElement("div");
+  var img = document.createElement("img");
+      img.setAttribute("src",url);
+      img.setAttribute("alt",nombre);
+  var span = document.createElement("span");
+  var text = document.createTextNode(nombre);
+      span.classList.add("nombre");
+      span.appendChild(text);
+  var span2 = document.createElement("span");
+  div.appendChild(span2);
+  div.appendChild(img);
+  div.appendChild(span);
+  dcf.appendChild(div);
+}
+function createtitle(dcf,padre){
+  var section = document.createElement("section");
+  section.classList.add("photo");
+  section.appendChild(dcf);
+  var title = document.createElement("h1");
+  var text = document.createTextNode("StyleCoder");
+      title.appendChild(text);
+  var line = document.createElement("h2");
+  padre.appendChild(title);
+  padre.appendChild(line);
+  padre.appendChild(section);
+  console.log(padre);
+}
